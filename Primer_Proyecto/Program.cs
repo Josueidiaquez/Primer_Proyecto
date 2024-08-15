d59e30037f5d4a44436564ef6771059ded9abb2a
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-15 12:59:38 -0600

    Update Program.cs

--- Primer_Proyecto/Program.cs
+++ Primer_Proyecto/Program.cs
@@ -1,4 +1,9 @@
+﻿int Edad, EdadMeses;
 ﻿String Nombre;
 Console.WriteLine("Bienvenidos a nuestro sistema");
 Console.WriteLine("Ingrese su nombre para continuar");
-Nombre = Console.ReadLine();
+Nombre = Console.WriteLine();
+Console.WriteLine("Ingrese su Edad");
+Edad = Convert.ToInt32(Console.ReadLine);
+EdadMeses = Edad * 12;
+Console.WriteLine("La edad de meses es: ", EdadMeses);
