ad41dbf10498dcf1c7905fd519129c277d2acba0
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-15 13:48:45 -0600

    Update Program.cs

--- Primer_Proyecto/Program.cs
+++ Primer_Proyecto/Program.cs
@@ -1,11 +1,28 @@
-﻿
-int Edad, EdadMeses;
-string ﻿Nombre;
-Console.WriteLine("Bienvenidos a nuestro sistema");
-Console.WriteLine("Ingrese su nombre para continuar");
-Nombre = Console.ReadLine();
-Console.WriteLine("Ingrese su Edad");
-Edad = Convert.ToInt32(Console.ReadLine);
-EdadMeses = Edad * 12;
-Console.WriteLine(Nombre + "La edad de meses es: " + EdadMeses + "Meses");
-if (EdadMeses > 0) ;
+﻿using System;
+public class progra;
+{
+    static void Main(string[] args)
+    {
+        int Edad, EdadMeses;
+        string Nombre;
+        Console.WriteLine("Bienvenidos a nuestro sistema");
+        Console.WriteLine("Ingrese su nombre para continuar");
+        Nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese su Edad");
+        Edad = Convert.ToInt32(Console.ReadLine);
+        EdadMeses = Edad * 12;
+        Console.WriteLine(Nombre + "La edad de meses es: " + EdadMeses + "Meses");
+        if (EdadMeses > 0) ;
+        {
+            Console.WriteLine("Usted es Mayor de Edad");
+        }
+
+else
+        {
+            Console.WriteLine("Usted es Menor de Edad");
+        }
+    }
+    
+}
+
+
